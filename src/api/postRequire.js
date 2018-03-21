cc55896d5a65cd34b8daa378b0b69e680977bfce
wspandihai <2711002357@qq.com>
2018-03-21 17:02:41 +0800

    commit

--- src/api/postRequire.js
+++ src/api/postRequire.js
@@ -1,9 +1,15 @@
 import base from './base'
 
 export default class postRequire extends base {
-  static async post(requireData = {}) {
+  static async postData(requireData = {}) {
     const url = ''
     const result = await this.put(url, requireData)
+    return result
+  }
+
+  static async list(params = {}) {
+    const url = 'http://mock.eolinker.com/td1pdjj6f0d1910c659b334de1e929b431ab8dc2c0e09fc?uri=/freight/list'
+    const result = await this.post(url, params)
     return result
   }
 }
